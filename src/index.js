40556d8f09c1bf8b4c356910e86734e8135160f7
lyskoivan <lysko1999i@gmail.com>
2019-12-21 10:27:50 +0200

    dsa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import 'normalize.css';
 
-import App from './components/App';
+import App from './Components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
